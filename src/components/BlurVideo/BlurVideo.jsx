79e4a96165e71977c497657ab7880ca07fb8c59c
eze stom <ezequielstom@gmail.com>
2024-02-08 19:28:53 -0300

    Update UI elements and translations

--- src/components/BlurVideo/BlurVideo.jsx
+++ src/components/BlurVideo/BlurVideo.jsx
@@ -5,50 +5,48 @@ import close from "../../icons/x.svg";
 import "./BlurVideo.css";
 
 export function BlurVideo() {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const openDialog = () => {
-		setIsOpen(true);
-		// stop scroll
-		// document.body.style.overflow = "hidden";
-	};
-	const closeDialog = () => {
-		setIsOpen(false);
-		// enable scroll
-		// document.body.style.overflow = "auto";
-	};
+  const openDialog = () => {
+    setIsOpen(true);
+    // stop scroll
+    // document.body.style.overflow = "hidden";
+  };
+  const closeDialog = () => {
+    setIsOpen(false);
+    // enable scroll
+    // document.body.style.overflow = "auto";
+  };
 
-	return (
-		<article className="relative max-w-full md:max-w-[85%] article-video">
-			<button onClick={openDialog}>
-			<video
-				className="rounded-[4rem] shadow-md  app-video w-full opacity-90"
-				src={video1}
-				loop
-				muted
-				autoPlay></video>
-		
-			</button>
-			{isOpen && (
-				<dialog open className="z-70">
-					<iframe
-						className="m-auto w-full p-1 md:w-[50%] aspect-video rounded-lg shadow-2xl"
-						src="https://www.youtube.com/embed/XTH7oeiY2Cg?si=fDqlXkuYmHE4XKDi&autoplay=1"
-						title="YouTube video player"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-						allowFullScreen></iframe>
+  return (
+    <article className=" max-w-full md:max-w-[85%] article-video">
+      <button onClick={openDialog}>
+        <video
+          className="rounded-[4rem] shadow-md  app-video w-full opacity-90"
+          src={video1}
+          loop
+          muted
+          autoPlay
+        ></video>
+      </button>
+      {isOpen && (
+        <dialog open className="z-50 fixed h-screen w-screen top-0 left-0">
+          <iframe
+            className="m-auto bottom-0 top-0 left-0 right-0 absolute w-full p-1 md:w-[50%] aspect-video rounded-[3rem] shadow-2xl"
+            src="https://www.youtube.com/embed/XTH7oeiY2Cg?si=fDqlXkuYmHE4XKDi&autoplay=1"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
 
-					<button
-						className="fixed top-20  z-80"
-						onClick={closeDialog}>
-						<img
-							src={close.src}
-							alt="close button"
-							className="close-button"
-						/>
-					</button>
-				</dialog>
-			)}
-		</article>
-	);
+          <button
+            className="fixed top-10 bg-gradient-to-r from-[#030f33] to-[#4e737a] rounded-full  z-80 p-1 close-button "
+            onClick={closeDialog}
+          >
+            <img src={close.src} alt="close button" />
+          </button>
+        </dialog>
+      )}
+    </article>
+  );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import x from "../../icons/x.svg";
+import "./Form.css";
+
+export function Form() {
+  const [dialog, setDialog] = useState(false);
+
+  const openDialog = () => {
+    setDialog(true);
+    // hacer funcion para bloquear scroll
+    // document.body.style.overflow = "hidden";
+  };
+
+  const closeDialog = () => {
+    setDialog(false);
+    // hacer funcion para desbloquear scroll
+    // document.body.style.overflow = "auto";
+  };
+
+  return (
+    <div className="relative form-container  z-10  ">
+      <h2 className="text-xl font-bold"> ¿Preparado para comenzar?​ </h2>
+      <hr className="hr-contact" />
+      <span className="flex pt-2">
+        <a
+          href="#open-dialog"
+          onClick={openDialog} // Usar la función openDialog aquí
+          id="open-dialog"
+          class="flex h-9 w-full items-center justify-center px-4 rounded-full transition duration-300 hover:scale-105 bg-gradient-to-r from-[#030f33] to-[#4e737a] active:duration-75 active:scale-95 sm:w-max text-white font-bold "
+        >
+          Ir al formulario
+          <svg
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            aria-hidden="true"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            ></path>
+          </svg>
+        </a>
+      </span>
+
+      {dialog && ( // Mostrar el diálogo solo cuando dialog es true
+        <dialog open>
+          <div className="form-container-dialog">
+            <form
+              method="POST"
+              action="https://formsubmit.co/ezequielstom@gmail.com"
+            >
+              <input
+                type="hidden"
+                name="_next"
+                value="https://yourdomain.co/thanks.html"
+              ></input>
+              <input type="hidden" name="_captcha" value="false"></input>
+              <legend>
+                <a
+                  className="flex justify-center my-2"
+                  href="/#contact"
+                  onClick={closeDialog}
+                >
+                  {" "}
+                  {/* Usar la función closeDialog aquí */}
+                  <img
+                    className="bg-black/30 rounded-xl p-2 hover:bg-black/20 transition "
+                    src={x.src}
+                    alt="x-icon"
+                    id="close-dialog"
+                  />
+                </a>
+              </legend>
+
+              <div className="flex flex-col gap-4 min-w-[350px] ">
+                <label htmlFor="name">Nombre</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="John Doe"
+                  className="rounded-md "
+                  required
+                />
+                <label htmlFor="email">Correo</label>
+                <input
+                  className="rounded-md "
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="john_doe@ejemplo.com"
+                  required
+                />
+                <label htmlFor="message">Mensaje</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Quiero unirme a la lista para recibir noticias sobre el lanzamiento de la App."
+                  required
+                ></textarea>
+                <button
+                  className="w-full bg-white/20 rounded-md py-1 hover:bg-white/30"
+                  type="submit"
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
+          </div>
+        </dialog>
+      )}
+    </div>
+  );
+}
