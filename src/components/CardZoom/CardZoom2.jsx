51bc9ff58159dd7fd1ba69f7d4b2c2330252efcb
eze stom <ezequielstom@gmail.com>
2024-03-01 10:45:22 -0300

    Update package.json, CardZoom2.jsx, and Layout.astro

--- src/components/CardZoom/CardZoom2.jsx
+++ src/components/CardZoom/CardZoom2.jsx
@@ -88,7 +88,7 @@ export function CardZoom2({ image, icon, title, description, video1 }) {
               >
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#030f33] to-[#4e737a] px-8 py-1  font-medium text-gray-50 backdrop-blur-3xl">
-                  Atrás
+                  Cerrar
                 </span>
               </a>
             </span>
