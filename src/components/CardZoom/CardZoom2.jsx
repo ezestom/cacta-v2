9113d5dcc30669e9cdeac397bf164ff5d35200b1
ezequiel stom <ezequielstom@gmail.com>
2024-10-10 20:17:48 -0300

    add new companies partners

--- src/components/CardZoom/CardZoom2.jsx
+++ src/components/CardZoom/CardZoom2.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import close from "../../icons/x.svg";
 
 export function CardZoom2({ image, icon, title, description, video1 }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,10 +23,9 @@ export function CardZoom2({ image, icon, title, description, video1 }) {
   };
 
   return (
-    <section className="max-w-screen w-full  h-max-full md:max-h-screen ">
+    <section className="max-w-screen w-full  h-max-full md:max-h-screen">
       <div className="flex justify-center flex-wrap  gap-5 px-2  md:p-0 w-full">
         <div className=" rounded-[3rem] border border-gray-100  bg-white/30 dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/40 dark:shadow-none backdrop-blur-xl p-4  w-full max-w-[500px] ">
-          {/* <img src={icon} alt="icon" className="w-10 absolute top-3 right-3" /> */}
           <div className="mx-auto h-full flex justify-center flex-col items-center text-start lg:text-left ">
             <header className="w-full ">
               <h2 className="text-xl text-center font-bold text-gray-900 sm:text-2xl dark:text-white uppercase">
@@ -46,7 +46,7 @@ export function CardZoom2({ image, icon, title, description, video1 }) {
           </div>
         </div>
 
-        <figure className=" z-20  ">
+        <figure className="z-[60]">
           {video1 ? (
             <button onClick={openDialog}>
               <video
@@ -54,7 +54,7 @@ export function CardZoom2({ image, icon, title, description, video1 }) {
                 loop
                 muted
                 src={video1}
-                className=" aspect-auto w-full max-w-xl rounded-[3rem] shadow-2xl transition-all "
+                className=" aspect-auto w-full max-w-xl rounded-[3rem] shadow-2xl transition-all"
               ></video>
             </button>
           ) : undefined}
@@ -77,8 +77,8 @@ export function CardZoom2({ image, icon, title, description, video1 }) {
                 className=" relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50"
               >
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#030f33] to-[#4e737a] px-8 py-1  font-medium text-gray-50 backdrop-blur-3xl">
-                  Cerrar
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#030f33] to-[#4e737a] p-2 z-50 font-medium text-gray-50 backdrop-blur-3xl">
+                  <img src={close.src} alt="close-icon" />
                 </span>
               </a>
             </span>
